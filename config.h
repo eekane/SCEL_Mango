42be4fb87af5ca0a6a8f329fed6f5ee1a81bd1e3
eekane <31784109+eekane@users.noreply.github.com>
2017-11-22 16:46:41 -1000

    Update config.h

--- config.h
+++ config.h
@@ -1,25 +1,25 @@
 /* File: config.h */
 
 /* SHT11 Humidity */
-#define PIN_HUMID_CLK 13
-#define PIN_HUMID_DATA 14
+#define PIN_HUMID_CLK 7
+#define PIN_HUMID_DATA 8
 
 /* SP215 Solar Irradiance */
-#define PIN_APOGEE_V 25
+#define PIN_APOGEE_V A2
 
 /* Panel and Battery Voltage */
-#define PIN_PANEL_V 24
-#define PIN_BATT_V 23
+#define PIN_PANEL_V A1
+#define PIN_BATT_V A0
 
 /* XBEE */
-#define PIN_XBEE_TX 2
-#define PIN_XBEE_RX 3
-#define PIN_XBEE_SLEEP 9
+#define PIN_XBEE_TX 0
+#define PIN_XBEE_RX 1
+#define PIN_XBEE_SLEEP 13
 
 /* Receiver/Transmitter */
 #define PIN_RX 0
 #define PIN_TX 1
 
  /* BMP280 Pressure and Temperature */
- #define PIN_SDA 27
- #define PIN_SCL 28
+ #define PIN_SDA 18
+ #define PIN_SCL 19
